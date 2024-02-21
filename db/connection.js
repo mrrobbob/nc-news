5ddaf05e5b0cf5e5a5835cab26ae1fb60358def2
root <mr.robbob@hotmail.co.uk>
2024-02-21 09:44:40 +0000

    changed prepare in json

--- db/connection.js
+++ db/connection.js
@@ -11,8 +11,8 @@ if (ENV === 'production') {
   config.max = 2
 }
 
-if (!process.env.PGDATABASE && !process.nextTick.DATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
